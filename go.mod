41b94c783f1ee0e68d87f338202c0a68e7725fde
qubicfire <burpilav@gmail.com>
2026-05-02 12:14:58 +0300

    some changes

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module src/bootstrap/app.go
+module github.com/qubicfire/test-bank-system
 
 go 1.22.2
--- src/controllers/login_controller.go
+++ src/controllers/login_controller.go
@@ -1,1 +1,21 @@
 package controllers
+
+import (
+	"errors"
+)
+
+type LoginAdapter interface {
+	send() error
+}
+
+type TelegramLoginAdapter struct {
+}
+
+func (adapter *TelegramLoginAdapter) send() error {
+	var err = errors.New("Not implemented")
+	return err
+}
+
+func SendLoginCode(adapter LoginAdapter) {
+	adapter.send()
+}
